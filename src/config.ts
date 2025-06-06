01880923e31c0b880f75a0fceacfd3ecf7fd3465
saiapowainur <saiapow.ainur@gmail.com>
2025-06-06 11:19:38 +0400

    feat: rename projects to products

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ export const header = {
   logoLink: meeFoundation,
   links: [
     {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
+      name: "Products",
+      link: `${meeFoundation}products/`,
+      id: "products",
     },
     {
       name: "Businesses",
@@ -68,9 +68,9 @@ export const footer = {
   ],
   rightLinks: [
     {
-      name: "Projects",
-      link: `${meeFoundation}projects/`,
-      id: "projects",
+      name: "Products",
+      link: `${meeFoundation}products/`,
+      id: "products",
     },
     {
       name: "Businesses",
